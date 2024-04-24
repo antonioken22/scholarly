71cb1b78798b2c7933c5a25a41bc50d24d9aeaa2
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-04-24 21:20:07 +0800

    Made the color uniform in dark mode [#1F1F1F]

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -22,7 +22,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return redirect("/");
   }
   return (
-    <div className="h-full flex dark:bg-[#1f1f1f]">
+    <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./logo";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-ful p-6 bg-background z-50 dark:bg-[#1f1f1f]">
+    <div className="flex items-center w-ful p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
         <Button variant="ghost" size="sm">
