8afe406fd200dc0316edda86a6199c292aab2e60
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-04-27 18:32:13 +0800

    Fixed the position of Publish and Menu buttons on md+ devices and Made it not visible when the notes is archived

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -52,10 +52,12 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
-          <div className="flex items-center gap-x-2">
-            <Publish initialData={document} />
-            <Menu documentId={document._id} />
-          </div>
+          {!document.isArchived && (
+            <div className="md:fixed md:top-0 md:right-0 md:m-1 flex items-center gap-x-2">
+              <Publish initialData={document} />
+              <Menu documentId={document._id} />
+            </div>
+          )}
         </div>
       </nav>
       {document.isArchived && ((isMobile && isCollapsed) || !isMobile) && (
