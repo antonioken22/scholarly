cab61eb933f2f9e804978638a264bbe826c89865
Kenette John A. Antonio <antonio.kenjohn15@gmail.com>
2024-04-27 16:05:56 +0800

    Set title holder from navbar max width to 240px on all devices with truncate

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -48,7 +48,7 @@ export const Title = ({ initialData }: TitleProps) => {
   };
 
   return (
-    <div className="flex items-center gap-x-1">
+    <div className="flex items-center gap-x-1 max-w-[240px]">
       {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
         <Input
@@ -65,7 +65,7 @@ export const Title = ({ initialData }: TitleProps) => {
           onClick={enableInput}
           variant="ghost"
           size="sm"
-          className="font-normal h-auto p-1"
+          className="truncate font-normal h-auto p-1"
         >
           <span className="truncate">{initialData?.title}</span>
         </Button>
